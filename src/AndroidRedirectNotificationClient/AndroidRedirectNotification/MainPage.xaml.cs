d23f7675084cf3e0b8b7e17fa94fadf30841ab6b
Lionel-Kyo <68594220+Lionel-Kyo@users.noreply.github.com>
2026-09-02 23:20:58 +0900

    Update MainPage.xaml.cs

--- src/AndroidRedirectNotificationClient/AndroidRedirectNotification/MainPage.xaml.cs
+++ src/AndroidRedirectNotificationClient/AndroidRedirectNotification/MainPage.xaml.cs
@@ -1,122 +1,123 @@
 ﻿using System.Collections.ObjectModel;
 using System.Net;
 
-namespace AndroidRedirectNotification;
-
-public partial class MainPage : ContentPage
+namespace AndroidRedirectNotification
 {
-    public static MainPage? Instance { get; private set; }
-    public static string ServerIp { get; private set; } = "192.168.1.1";
-    public static ushort ServerPort { get; private set; } = 443;
-
-    public Entry IpEditEntry => ipEditEntry;
-    public Entry PortEditEntry => portEntry;
-
-    public ObservableCollection<NotificationLog> Notifications { get; set; } = new();
-
-    public MainPage()
+    public partial class MainPage : ContentPage
     {
-        InitializeComponent();
-        Instance = this;
+        public static MainPage? Instance { get; private set; }
+        public static string ServerIp { get; private set; } = "192.168.1.1";
+        public static ushort ServerPort { get; private set; } = 443;
 
-        historyCollectionView.ItemsSource = Notifications;
+        public Entry IpEditEntry => ipEditEntry;
+        public Entry PortEditEntry => portEntry;
 
-        ServerIp = Preferences.Get("server_ip", "192.168.1.1");
-        ServerPort = (ushort)Preferences.Get("server_port", 443);
-        this.ipEditEntry.Text = ServerIp;
-        this.portEntry.Text = ServerPort.ToString();
-    }
+        public ObservableCollection<NotificationLog> Notifications { get; set; } = new();
 
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-    }
-
-    public void AddNotification(string message, string title, DateTimeOffset utcDateTime)
-    {
-        MainThread.BeginInvokeOnMainThread(() =>
+        public MainPage()
         {
-            Notifications.Insert(0, new NotificationLog
+            InitializeComponent();
+            Instance = this;
+
+            historyCollectionView.ItemsSource = Notifications;
+
+            ServerIp = Preferences.Get("server_ip", "192.168.1.1");
+            ServerPort = (ushort)Preferences.Get("server_port", 443);
+            this.ipEditEntry.Text = ServerIp;
+            this.portEntry.Text = ServerPort.ToString();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        public void AddNotification(string message, string title, DateTimeOffset utcDateTime)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
             {
-                Title = title,
-                Message = message,
-                Timestamp = utcDateTime.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss")
+                Notifications.Insert(0, new NotificationLog
+                {
+                    Title = title,
+                    Message = message,
+                    Timestamp = utcDateTime.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss")
+                });
             });
-        });
-    }
-
-    public static void LogNotification(string message, string title, DateTimeOffset utcDateTime)
-    {
-        Instance?.AddNotification(message, title, utcDateTime);
-    }
-
-    private static bool IsValidIpAddress(string input)
-    {
-        return IPAddress.TryParse(input, out _);
-    }
-
-    private static bool IsValidHostName(string input)
-    {
-        try
-        {
-            var addresses = Dns.GetHostAddresses(input);
-            return addresses.Length > 0;
-        }
-        catch { }
-        return false;
-    }
-
-    private async void OnEditBtnClicked(object sender, EventArgs e)
-    {
-        string newIp = this.ipEditEntry.Text;
-        string newPort = this.portEntry.Text;
-
-        if (!IsValidIpAddress(newIp) && !IsValidHostName(newIp))
-        {
-            await DisplayAlert("Server Configuration", "Invalid ip or host.", "OK");
-            return;
         }
 
-        if (!ushort.TryParse(newPort, out ushort u16Port) || u16Port == 0)
+        public static void LogNotification(string message, string title, DateTimeOffset utcDateTime)
         {
-            await DisplayAlert("Server Configuration", "Invalid port.", "OK");
-            return;
+            Instance?.AddNotification(message, title, utcDateTime);
         }
 
-        ServerIp = newIp;
-        ServerPort = u16Port;
-        Preferences.Set("server_ip", newIp);
-        Preferences.Set("server_port", u16Port);
-
-        await DisplayAlert("Server Configuration", "Applied server information.", "OK");
-    }
-
-    private async void OnClearHistoryClicked(object sender, EventArgs e)
-    {
-        if (Notifications.Count == 0)
+        private static bool IsValidIpAddress(string input)
         {
-            await DisplayAlert("Clear History", "No notification record.", "OK");
+            return IPAddress.TryParse(input, out _);
         }
-        else
-        {
-            bool isConfirmed = await DisplayAlert(
-                "Clear History",
-                "Are you sure you want to delete all notification records?",
-                "Clear",
-                "Cancel"
-            );
 
-            if (isConfirmed)
+        private static bool IsValidHostName(string input)
+        {
+            try
             {
-                Notifications.Clear();
+                var addresses = Dns.GetHostAddresses(input);
+                return addresses.Length > 0;
+            }
+            catch { }
+            return false;
+        }
+
+        private async void OnEditBtnClicked(object sender, EventArgs e)
+        {
+            string newIp = this.ipEditEntry.Text;
+            string newPort = this.portEntry.Text;
+
+            if (!IsValidIpAddress(newIp) && !IsValidHostName(newIp))
+            {
+                await DisplayAlert("Server Configuration", "Invalid ip or host.", "OK");
+                return;
+            }
+
+            if (!ushort.TryParse(newPort, out ushort u16Port) || u16Port == 0)
+            {
+                await DisplayAlert("Server Configuration", "Invalid port.", "OK");
+                return;
+            }
+
+            ServerIp = newIp;
+            ServerPort = u16Port;
+            Preferences.Set("server_ip", newIp);
+            Preferences.Set("server_port", u16Port);
+
+            await DisplayAlert("Server Configuration", "Applied server information.", "OK");
+        }
+
+        private async void OnClearHistoryClicked(object sender, EventArgs e)
+        {
+            if (Notifications.Count == 0)
+            {
+                await DisplayAlert("Clear History", "No notification record.", "OK");
+            }
+            else
+            {
+                bool isConfirmed = await DisplayAlert(
+                    "Clear History",
+                    "Are you sure you want to delete all notification records?",
+                    "Clear",
+                    "Cancel"
+                );
+
+                if (isConfirmed)
+                {
+                    Notifications.Clear();
+                }
             }
         }
     }
-}
 
-public class NotificationLog
-{
-    public string Title { get; set; } = "";
-    public string Message { get; set; } = "";
-    public string Timestamp { get; set; } = "";
+    public class NotificationLog
+    {
+        public string Title { get; set; } = "";
+        public string Message { get; set; } = "";
+        public string Timestamp { get; set; } = "";
+    }
 }
